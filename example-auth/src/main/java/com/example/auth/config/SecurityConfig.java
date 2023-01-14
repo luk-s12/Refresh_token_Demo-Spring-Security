f8ec1e98a4c4772aa09cf88f7c7af3fbad37ee54
luk-s12 <lucas.utn12@gmail.com>
2023-01-13 23:15:02 -0300

    refactor: cambio el tipo de la firma del bean para cifrar el password  y organizo el yml

--- example-auth/src/main/java/com/example/auth/config/SecurityConfig.java
+++ example-auth/src/main/java/com/example/auth/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -40,7 +41,7 @@ public class SecurityConfig {
     }
 	
 	@Bean
-	BCryptPasswordEncoder bCryptPasswordEncoder() {
+	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}	
 	
